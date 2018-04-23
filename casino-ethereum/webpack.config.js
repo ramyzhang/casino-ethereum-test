cdeaac11ca7c356ece72020c492b0450afd00992
ramyzhang <34625981+ramyzhang@users.noreply.github.com>
2018-04-23 11:03:36 -0400

    Update webpack.config.js

--- casino-ethereum/webpack.config.js
+++ casino-ethereum/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+
 module.exports = {
    entry: path.join(__dirname, 'src/js', 'index.js'), // Our frontend will be inside the src folder
    output: {
@@ -6,7 +7,7 @@ module.exports = {
       filename: 'build.js' // The final file will be created in dist/build.js
    },
    module: {
-      loaders: [{
+      rules: [{
          test: /\.css$/, // To load the css in react
          use: ['style-loader', 'css-loader'],
          include: /src/
